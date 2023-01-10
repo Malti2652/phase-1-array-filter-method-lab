65a85f5bea456f62a52fc505dbe49c535d5175c4
Eddington Rowellis <eddingtonibi@gmail.com>
2023-01-10 22:45:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, Bobby, Sammy, Sally, Annette, Sarah) {
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+module.exports = { findMatching }
+
+
+
+function fuzzyMatch(drivers, Bobby, Sammy, Sally) {
+  return drivers.filter(driver => driver.toLowerCase().startsWith(name.toLowerCase()));
+}
+
+module.exports = { fuzzyMatch }
+
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+}
+
+module.exports = { matchName }
+
